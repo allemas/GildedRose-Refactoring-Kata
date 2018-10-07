17966429cfe66f50c063360d16a647bd5719bb79
Allemand Sébastien <sebastienallemand.as@gmail.com>
2018-10-07 21:37:02 +0200

    initialisation test

--- php/test/gilded_rose_test.php
+++ php/test/gilded_rose_test.php
@@ -2,13 +2,50 @@
 
 require_once 'gilded_rose.php';
 
-class GildedRoseTest extends PHPUnit_Framework_TestCase {
+use PHPUnit\Framework\TestCase;
 
-    function testFoo() {
+class GildedRoseTest extends TestCase
+{
+
+    function testFoo()
+    {
         $items = array(new Item("foo", 0, 0));
         $gildedRose = new GildedRose($items);
         $gildedRose->update_quality();
-        $this->assertEquals("fixme", $items[0]->name);
+        $this->assertEquals("foo", $items[0]->name);
     }
+
+    /**
+     * All items have a SellIn value which denotes the number of days we have to sell the item
+     */
+    function testSellInValue()
+    {
+        $this->assertClassHasAttribute("sell_in", Item::class);
+    }
+
+    /**
+     *    - All items have a Quality value which denotes how valuable the item is
+     */
+    function testQualityValue()
+    {
+        $this->assertClassHasAttribute("quality", Item::class);
+    }
+
+    /**
+     * At the end of each day our system lowers both values for every item
+     */
+    function testSystemUpdateSellEvolution()
+    {
+        $sellIn = 10;
+        $quality = 20;
+        $items = array(new Item("foo", $sellIn, $quality));
+        $gilledRose = new GildedRose($items);
+        $gilledRose->update_quality();
+        $this->assertEquals($sellIn - 1, $items[0]->sell_in);
+        $this->assertEquals($quality - 1, $items[0]->quality);
+    }
+
+
+
 
 }
